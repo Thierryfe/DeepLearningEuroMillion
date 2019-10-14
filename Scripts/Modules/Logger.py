250a0fb060d3e77ad7135e1532eada8901e94da8
ThieF <thierry.fernandez@etud.univ-angers.fr>
2019-10-14 16:28:20 +0200

    Ajout des commentaires sur les divers fichiers de l'architecture

---
+++ Scripts/Modules/Logger.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+'''
+Auteur description : Thierry Fernandez
+Date modification : 14/10/20
+Description :
+Script ayant pour objectif de logger toutes les erreurs/warnings dans un fichier de logs. Avec niveau d'erreur, origine de l'erreur etc...
+
+Developpeur :
+'''
+
+
---
+++ Scripts/Modules/Updater.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+'''
+Auteur description : Thierry Fernandez
+Date modification : 14/10/20
+Description :
+Script ayant pour objectif de verifier si les données sont à jour, si ce n'est pas le cas, il faut les mettre à jour
+Developpeur :
+'''
