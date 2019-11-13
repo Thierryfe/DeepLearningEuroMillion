de454c01598f2a1a2660e7d09fe0775b8742aa89
CChaudeurdy <chaudeurdy.corentin@hotmail.fr>
2019-11-13 18:00:01 +0100

    revue de code

--- Scripts/Modules/Parsing.py
+++ Scripts/Modules/Parsing.py
@@ -10,15 +10,16 @@ import numpy as np
 import os.path
 import csv
 
-def setDataForTensorflow(annee_de_debut=2004,anne_de_fin=2005):
-    resultat = np.matrix([[]])
+def setDataForTensorflow(annee_de_debut=2004,annee_de_fin=2005):
+    resultat = np.matrix([[]],int)
     pathDataCSV = os.path.expanduser("~") + "/Documents/DataTirage" + "/dataCSV"
     countTurnOne = 1
-    for i in range(annee_de_debut, anne_de_fin + 1):
+    for i in range(annee_de_debut, annee_de_fin + 1):
         with open(pathDataCSV + '/euromillions_' + str(i) + '.csv', 'r') as f:
             reader = csv.reader(f)
             for row in reader:
-                rowNumPy = np.matrix([ [row[1],row[2],row[3],row[4],row[5],row[6], row[7] ] ])
+
+                rowNumPy = np.matrix( [ [ int(row[1]), int(row[2]),int(row[3]),int(row[4]),int(row[5]),int(row[6]),int(row[7]) ] ] )
 
                 if (countTurnOne==1):
 
@@ -28,3 +29,5 @@ def setDataForTensorflow(annee_de_debut=2004,anne_de_fin=2005):
                     resultat = np.concatenate((resultat, rowNumPy), axis=0)
 
     return resultat
+
+print(setDataForTensorflow())
--- Scripts/Modules/Updater.py
+++ Scripts/Modules/Updater.py
@@ -11,28 +11,33 @@ import os.path
 import xlrd
 import csv
 from urllib.request import urlretrieve
+import datetime
 
+def downloadFile(annee_de_debut=2004,annee_de_fin=2005,url="http://www.lottology.com/europe/euromillions/?do=past-draws-archive&tab=&as=XLS&year="):
+    if(annee_de_debut<=annee_de_fin):
+        if((annee_de_debut>=2004)and(annee_de_fin<= datetime.datetime.now().year)):
+            pathData = os.path.expanduser("~") + "/Documents/DataTirage"
+            pathFilesXLS = pathData + "/dataXLS"
+            if not (os.path.isdir(pathFilesXLS)):
+                os.makedirs(pathFilesXLS)
 
-def dowloadFile(anne_de_debut=2004,anne_de_fin=2005,url="http://www.lottology.com/europe/euromillions/?do=past-draws-archive&tab=&as=XLS&year="):
-    pathData = os.path.expanduser("~") + "/Documents/DataTirage"
-    pathFilesXLS = pathData + "/dataXLS"
-    if not (os.path.isdir(pathFilesXLS)):
-        os.makedirs(pathFilesXLS)
-
-    for i in range(anne_de_debut, anne_de_fin):
-        fname = "euromillions_" + str(i) + ".xls"
-        if not (os.path.isfile(pathFilesXLS + "/" + fname)):
-            urlretrieve(
-                url + str(i) + " ",
-                "" + pathFilesXLS + '/' + fname)
-    convertXLStoCSV(pathData,pathFilesXLS,anne_de_debut,anne_de_fin)
-
-def convertXLStoCSV(pathData,pathDataXLS, anne_de_debut,anne_de_fin):
+            for i in range(annee_de_debut, annee_de_fin+1):
+                fname = "euromillions_" + str(i) + ".xls"
+                if not (os.path.isfile(pathFilesXLS + "/" + fname)):
+                    urlretrieve(
+                        url + str(i) + " ",
+                        "" + pathFilesXLS + '/' + fname)
+            convertXLStoCSV(pathData,pathFilesXLS,annee_de_debut,annee_de_fin)
+        else:
+            print("pas de tirages presents pour annee_de_debut et annee_de_fin")
+    else:
+        print("annee_de_debut doit etre anterieure a annee_de_fin")
+def convertXLStoCSV(pathData,pathDataXLS, annee_de_debut,annee_de_fin):
     pathDataCSV = pathData + "/dataCSV"
     if not(os.path.isdir(pathDataCSV)):
         os.makedirs(pathDataCSV)
 
-    for i in range(anne_de_debut, anne_de_fin):
+    for i in range(annee_de_debut, annee_de_fin+1):
         fname = 'euromillions_' + str(i) + '.xls'
         wb = xlrd.open_workbook(pathDataXLS+"/"+ fname)
         sh = wb.sheet_by_index(0)
@@ -41,12 +46,11 @@ def convertXLStoCSV(pathData,pathDataXLS, anne_de_debut,anne_de_fin):
         wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
         for rownum in range(sh.nrows):
             if(rownum>4):
-                row = [sh.row_values(rownum)[0] , int(float(sh.row_values(rownum)[1])), int(float(sh.row_values(rownum)[2])) ,int(float(sh.row_values(rownum)[3])) ,int(float(sh.row_values(rownum)[4])),int(float(sh.row_values(rownum)[5])), int(float(sh.row_values(rownum)[6])) , int(float(sh.row_values(rownum)[7]))  ]
-                print(row)
+                row = [int(sh.row_values(rownum)[0]) , int(float(sh.row_values(rownum)[1])), int(float(sh.row_values(rownum)[2])) ,int(float(sh.row_values(rownum)[3])) ,int(float(sh.row_values(rownum)[4])),int(float(sh.row_values(rownum)[5])), int(float(sh.row_values(rownum)[6])) , int(float(sh.row_values(rownum)[7]))  ]
                 wr.writerow(row)
 
         your_csv_file.close()
 
 #pour tester la fonction dowloadFile(2004,2020) et recuperer tout les fichier xls et csv
-dowloadFile(2004,2005)
+downloadFile(2004,2004)
 
