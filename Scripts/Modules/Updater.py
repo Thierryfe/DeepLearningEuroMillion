b2393851d4f3f823552447d37b3a5d18e5e9a634
abzechix <abderrahimzeghichi@gmail.com>
2019-11-13 17:12:07 +0100

    fonctionnalites checkupdate() et Update()

--- Scripts/Modules/Updater.py
+++ Scripts/Modules/Updater.py
@@ -10,8 +10,9 @@ import requests
 import os.path
 import xlrd
 import csv
+import datetime
 from urllib.request import urlretrieve
-
+from urllib.request import urlopen
 
 def dowloadFile(anne_de_debut=2004,anne_de_fin=2005,url="http://www.lottology.com/europe/euromillions/?do=past-draws-archive&tab=&as=XLS&year="):
     pathData = os.path.expanduser("~") + "/Documents/DataTirage"
@@ -48,5 +49,63 @@ def convertXLStoCSV(pathData,pathDataXLS, anne_de_debut,anne_de_fin):
         your_csv_file.close()
 
 #pour tester la fonction dowloadFile(2004,2020) et recuperer tout les fichier xls et csv
-dowloadFile(2004,2005)
+#dowloadFile(2004,2005)
+def checkUpdate(url=lien_par_defaut):
+    # recuperation de la date du dernier tirage sauvegardé sur le fichier de l'année actuelle
+    file_path = data_dir + "/euromillions_" + str(datetime.date.today().year) + ".csv"
+    with open(file_path, newline='') as fichier_annee_courante:
+        reader = csv.reader(fichier_annee_courante, delimiter=',', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            dernier_tirage = row[0]
+            break
+
+    date_dernier_tirage = datetime.datetime.strptime(dernier_tirage, '%d %B %Y').date()
+
+    # recupération de tous les tags td contenants les dates des tirages
+    uClient = urlopen(url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup = soup(page_html, "html.parser")
+    table = page_soup.findAll("td", {"class": "lightblue text-left nowrap date"})
+
+    # test et recuperation de toutes les dates manquantes et les mettre dans un tableau
+
+    i = 0
+    tableau_de_dates = []
+    while datetime.date.fromisoformat(table[i].a["href"][23:]) > date_dernier_tirage:
+        tableau_de_dates.append(table[i].a["href"][23:])
+        i = i + 1
+    return tableau_de_dates
+
+
+def update(tableau_de_dates=None, url=lien_par_defaut):
+    tableau_de_dates = chekUpdate()  # pour mettre à jour le tableau des dates mieux que passer un tableau de dates au parametres
+    if len(tableau_de_dates) > 0:  # si on a des dates manquantes
+        # recuperer la page web
+        uClient = urlopen(url)
+        page_html = uClient.read()
+        uClient.close()
+        page_soup = soup(page_html, "html.parser")
+        # recuperer tous les tirage manquants et les dates
+        trs = page_soup.findAll("tr")
+        trs = trs[6:]
+        tds = []
+        for j in range(0, len(tableau_de_dates)):
+            tdsm = []
+            tds_ = trs[j].findAll("td")
+            tdsm.append(tds_[0].a["title"][20:])
+            for i in range(1, 8):
+                tdsm.append(tds_[i].text)
+            tds.append(tdsm)
+        file_path = data_dir + "/euromillions_" + str(datetime.date.today().year) + ".csv"
+        with open(file_path, 'r+', newline="") as f:
+            content = f.read()
+            f.seek(0, 0)
+            writer = csv.writer(f)
+            writer.writerows(tds)
+            for line in content:
+                f.writelines(line)
+
+    else:
+        print("le fichier de l'année courante est à jour, il n'y a pas de nouveaux tirages. \n")
 
