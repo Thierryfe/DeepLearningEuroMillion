4bc5b4055bb7a3f1b72560b02f7fcc7f4bd532f8
Hyddrogene <corentin.behuet@etud.univ-angers.fr>
2019-12-02 11:59:07 +0100

    Ajout quelque commentaire

--- Scripts/Motors/Keras/NetworkRNN.py
+++ Scripts/Motors/Keras/NetworkRNN.py
@@ -11,13 +11,28 @@ import Scripts.Modules.saveAndDisplayResultKerasRnn as sa
 import Scripts.Modules.SetDataForKeras as sd
 import numpy as np
 
+# ----------------------------------------------------------------------------------------
+# ------------------------- CREATION FONCTION NETWORK RNN---------------------------------
+# ----------------------------------------------------------------------------------------
+
 def NetworkRNN(Nb_epoch,date1,date2):
+
+# ----------------------------------------------------------------------------------------
+
+# ---------------------------------------------
+# ---------- PREPARATION DONNEE ---------------
+# ---------------------------------------------
+
 # ---------------------------------------------
 #récupetration des données du jeu d'entrainement
 # ---------------------------------------------
 
     Y=sd.SetDataForKeras(date1,date2);
     tabresultat=sr.KerasSetResultat(Y)
+
+# ---------------------------------------------
+# Définition d'un tagname pour la lastlayer
+# ---------------------------------------------
     lastname="dernierecouche"
 
 # ---------------------------------------------
@@ -41,30 +56,82 @@ def NetworkRNN(Nb_epoch,date1,date2):
 # ---------------------------------------------
 #mise en forme pour la lecture par le reseaux de neurones
 # ---------------------------------------------
-
-    M1 = np.reshape(M1, (M1.shape[0], 1, M1.shape[1]))
+    M = np.reshape(M, (M.shape[0], 1, M.shape[1]))
+   # M1 = np.reshape(M1, (M1.shape[0], 1, M1.shape[1]))
 
 # ---------------------------------------------
-#Notre reseau de neurones, ici en rnn
+#----------- DEFINITION DU RESEAU -------------
+# ---------------------------------------------
+
+# ---------------------------------------------
+#On crée le model, on initialise un reseau de neurone vide
 # ---------------------------------------------
 
     model = Sequential()
+
+# ---------------------------------------------
+#Ajout couche entré en precisant le fait qu'il soit Rnn 
+#on regle les unités oour eviter des problémes de typages
+#On donne la dimension des données en entrée un tableau de taille 7
+#Avec comme fonction d'activation une relu
+# ---------------------------------------------
+
     model.add(SimpleRNN(units=32, input_shape=(1,7), activation="relu"))
+
+# ---------------------------------------------
+#Ajout d"une couche de neurone (layer) de taille 64 avec fonction d'activation une relu
+# ---------------------------------------------
+
     model.add(Dense(64, activation="relu"))
+
+# ---------------------------------------------
+#Création couche de taille 7, avec un nom pour pouvoir récupérer les résulats après.
+# ---------------------------------------------
+
     last=Dense(7,name=lastname)
+
+# ---------------------------------------------
+#On ajoute la couche au reseau 
+# ---------------------------------------------
+
     model.add(last)
+
+# ---------------------------------------------
+# On définit maintenant quelle sera la fonction de perte pour le model, 
+# et l'optimizer pour la vitesse, pas très important dans notre cas
+# ---------------------------------------------
+
     model.compile(loss='mean_squared_error', optimizer='rmsprop')
-    model.fit(M1,M, epochs=Nb_epoch, batch_size=16, verbose=2)
+
+# ---------------------------------------------
+#On passe à l'entrainement, on donne en premier le jeu d'entrainement d'entrée, et celui de sortis 
+# pour pouvoir corriger ces erreurs, on lui indiqie le nombre d'epoch correspondant au nombres
+# de fois ou il va faire les test, et le nombre de batch le nombre de fois ou il parcours le reseau avant
+# de faire les calculs de correction, permet d'éviter d'avoir trop de calcul
+# On peut le faire à 1 mais pour 100 epoch on est à 10mn d'attente
+# une bonne taille est 16 permettant pour 100, d'avoir un temps d'attente correct
+# ---------------------------------------------
+
+    model.fit(M,M1, epochs=Nb_epoch, batch_size=1, verbose=2)
+
+# ---------------------------------------------
+# --------- RECUPERATION RESULTAT -------------
+# ---------------------------------------------
 
 # ---------------------------------------------
 #permet de recuperer la dernière sortis du model
+# le input sers à choisir qu'elle model on va " ecouter"
+#et le output permet de recupérer la ligne avant le tagname correspondant
 # ---------------------------------------------
 
     intermediate_layer_model = Model(inputs=model.input,
                                  outputs=model.get_layer(lastname).output)
 
 # ---------------------------------------------
-#ici oon demande au reseau de neurone de nous fournir une réponse pour la donnée en entrée
+#on demande au reseau de neurone de nous fournir une réponse pour la donnée en entrée
+# on utilise la utilise la ligne audessus qui reprend le model precedent, mais lors de la prediction nous renvoie
+# en plus le tableau de valeur correspondant
+# W est le tirage le plus recent, car normalement les tableau se mettent à jour
 # ---------------------------------------------
 
     intermediate_output = intermediate_layer_model.predict(W)
@@ -74,6 +141,22 @@ def NetworkRNN(Nb_epoch,date1,date2):
 # ---------------------------------------------
 
     sa.saveAndDisplayResultKerasRnn(intermediate_output[0],"sauvegardefichierRNN.csv")
+
+# ---------------------------------------------
+# permet de récupérer le tirage apres appelle de la fonction
+# ---------------------------------------------
+
     return intermediate_output[0]
 
+# ---------------------------------------------
+# ---------------------------------------------
+# ----------------------------------------------------------------------------------------
+#finfonction
+
+# ---------------------------------------------
+# Exemple utilisation fonction , on fixe le nombre d'epo, les dates entre les tirages
+# ---------------------------------------------
+
 NetworkRNN(100,2004,2019)
+
+# ---------------------------------------------
