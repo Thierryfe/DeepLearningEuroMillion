d7521246726b3d128caccae2d5d341eb7bf2f453
Lusgem <florian.lherbeil@yahoo.fr>
2020-01-16 16:54:32 +0100

    Modification nommage

--- Scripts/EuroMillionsTestManager.py
+++ Scripts/EuroMillionsTestManager.py
@@ -16,19 +16,19 @@ from motors.tensorFlow.networks import *
 
 #Si il n'y a pas d'argument, on passe en mode manuel
 if(len(sys.argv)==1):
-	reponse = input('Voulez vous télécharger (ou mettre à jour) les données de l\'Euromillion (y/n) ')
-	moteur = input('Voulez vous utiliser tensorflow ou keras ?\n')
-	if(moteur=='tensorflow'):
+	download = input('Voulez vous télécharger (ou mettre à jour) les données de l\'Euromillion (y/n) ?')
+	framework = input('Voulez vous utiliser tensorflow ou keras ?\n')
+	if(framework=='tensorflow'):
 		print("tensorflow")
 		reseau='RNN'
-	elif(moteur=='keras'):
+	elif(framework=='keras'):
 		reseau = input('Quel réseau de neurones voulez vous entrainer ? RNN ou CNN\n')
 	nbIter = input('Combien voulez-vous d\'itérations ?\n')
 	dateDebut = input('Veuillez choisir l\'intervalle (date entre 2004 et l\'année courante) sur lequel vous voulez traviller\nDate de début ? ')
 	dateFin = input('Date de fin ? ')
 elif(len(sys.argv)==7):
-	reponse=sys.argv[1]
-	moteur=sys.argv[2]
+	download=sys.argv[1]
+	framework=sys.argv[2]
 	reseau=sys.argv[3]
 	nbIter=sys.argv[4]
 	dateDebut=sys.argv[5]
@@ -42,19 +42,19 @@ nbIter=int(nbIter)
 dateDebut=int(dateDebut)
 dateFin=int(dateFin)
 
-if(reponse=='y'):
+if(download=='y'):
 	update()
-elif(reponse=='n'):
+elif(download=='n'):
 	print('Les données ne vont pas être téléchargées (ou mise à jour)')
 else:
 	print('Les arguments sont incorrects')
 	sys.exit()
-if(moteur=='keras'):
+if(framework=='keras'):
 	if(reseau=='RNN'):
 		NetworkRNN(nbIter,dateDebut,dateFin)
 	elif(reseau=='CNN'):
 		NetworkCNN(nbIter,dateDebut,dateFin)
-elif(moteur=='tensorflow'):
+elif(framework=='tensorflow'):
 	if(reseau=='RNN'):
 		launch()
 	elif(reseau=='CNN'):
@@ -64,7 +64,7 @@ else:
 	sys.exit()
 
 
-#pour exécuter les script avec les paramètre update=yes moteur=keras reseau=RNN nombre d'iteration = 100 date_debut=2004 et date_fin=2020
+#pour exécuter les script avec les paramètre download=yes framework=keras reseau=RNN nombre d'iteration = 100 date_debut=2004 et date_fin=2020
 #python EuroMillionsTestManager.py y keras RNN 100 2004 2020
 
 
