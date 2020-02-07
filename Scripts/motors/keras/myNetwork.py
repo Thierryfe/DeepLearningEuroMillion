122b8e53b6cb5ae32f21fcf3bd263c413ca16466
Thibault <thib.condemine@outlook.com>
2020-02-07 11:46:52 +0100

    network update

--- Scripts/motors/keras/myNetwork.py
+++ Scripts/motors/keras/myNetwork.py
@@ -5,7 +5,9 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import tensorflow as tensorflow
+import json
 
+from datetime import datetime
 from sklearn import model_selection
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, SimpleRNN, Reshape, TimeDistributed, Embedding, GRU, Dropout
@@ -48,7 +50,7 @@ def data_form(year, to_year):
 
     return input_train, input_test, output_train, output_test
 
-def network(units = 50, epochs = 100, batch_size = 32):
+def network(units = 32, epochs = 128, batch_size = 32, year = 2004, to_year = 2020):
     input_train, input_test, output_train, output_test = data_form(2004, 2020)
 
     model = Sequential()
@@ -63,64 +65,96 @@ def network(units = 50, epochs = 100, batch_size = 32):
 
     model.add(TimeDistributed(Dense(units = 50, activation = "softmax")))
 
-    mcp_save = ModelCheckpoint("keras_model_weights.h5", save_best_only=True, monitor='loss', mode='min', verbose=0)
+    model_path = os.path.join(os.path.realpath("."), "models")
+    model_name = "model_" + str(units) + "_" + str(epochs) + "_" + str(batch_size) + ".h5"
+
+    mcp_save = ModelCheckpoint(os.path.join(model_path, model_name), save_best_only = True, monitor = 'loss', mode = 'min', verbose = 0)
 
     model.compile(loss = "categorical_crossentropy", optimizer = "adam", metrics = ["accuracy"])
 
-    plot_path = os.path.join(os.path.realpath("."), "plot")
-    plot_name = "model_" + str(units) + "_" + str(epochs) + "_" + str(batch_size)
+    plot_path = os.path.join(os.path.realpath("."), "plots")
+    plot_name = "model_" + str(units) + "_" + str(epochs) + "_" + str(batch_size) + ".png"
 
-    plot_model(model, to_file = plot_path + plot_name, show_shapes=True, show_layer_names=True, rankdir='TB')
+    plot_model(model, to_file = os.path.join(plot_path, plot_name), show_shapes = True, show_layer_names = True, rankdir = 'TB')
 
-    log = model.fit(input_train, output_train, epochs = epochs, batch_size = batch_size, callbacks=[mcp_save])
+    log = model.fit(input_train, output_train, epochs = epochs, batch_size = batch_size, callbacks = [mcp_save])
 
     log_history = log.history["acc"]
+
+    pyplot_path = os.path.join(os.path.realpath("."), "accuracys")
+
     plt.figure()
+    plt.title(label = plot_name)
     plt.plot(numpy.arange(len(log_history)), log_history, marker='o', color='b', label='accuracy')
     plt.xlabel("Epoch")
-    plt.ylabel("accuracy history")
+    plt.ylabel("Accuracy History")
     plt.grid()
-    plt.show()
+    plt.savefig(os.path.join(pyplot_path, plot_name))
 
-def evaluate():
-    numpy.set_printoptions(threshold=sys.maxsize)
+def evaluate(year = 2004, to_year = 2020):
+    input_train, input_test, output_train, output_test = data_form(year, to_year)
 
-    input_train, input_test, output_train, output_test = data()
+    path = os.path.join(os.path.realpath("."), "models")
 
-    model = tensorflow.keras.models.load_model("keras_model_weights.h5")
+    files = os.listdir(path)
 
-    pred = model.predict(input_test)
+    scores = []
 
-    score = model.evaluate(pred, output_test)
+    for f in files:
+        model = tensorflow.keras.models.load_model(os.path.join(path, f))
 
-    print(score)
+        pred = model.predict(input_test)
+
+        score = model.evaluate(pred, output_test)
+        
+        score.append(f)
+
+        scores.append(score)
+
+    scores_path = os.path.join(os.path.realpath("."), "scores")
+    scores_name = "scores_" + datetime.now().strftime("%Y_%m_%d-%H_%M_%S") + ".json"
+
+    with open(os.path.join(scores_path, scores_name), 'w') as outfile:
+        json.dump(str(scores), outfile)
 
 def predict():
-    numpy.set_printoptions(threshold=sys.maxsize)
-
     list_nb = number_list()
 
-    start = numpy.zeros((8,50))
+    path = os.path.join(os.path.realpath("."), "models")
+
+    files = os.listdir(path)
+
+    tirages = []
+
+    for f in files:
+        start = numpy.zeros((8,50))
     
-    start = numpy.expand_dims(start, axis=0)
-
-    model = tensorflow.keras.models.load_model("keras_model_weights.h5")
-
-    tirage = []
-
-    for i in range(8):
-        pred = model.predict(start, steps = 8)
-
-        number = numpy.argmax(pred[0][i]) + 1
-
-        tirage.append(number)
-
-        tmp = list_nb[number]
-
-        start = numpy.insert(start[0], i + 1, tmp, axis = 0)
-
-        start = start[:-1,:]
-
         start = numpy.expand_dims(start, axis=0)
 
-    print(tirage)
+        model = tensorflow.keras.models.load_model(os.path.join(path, f))
+
+        tirage = []
+
+        for i in range(8):
+            pred = model.predict(start, steps = 8)
+
+            number = numpy.argmax(pred[0][i]) + 1
+
+            tirage.append(number)
+
+            tmp = list_nb[number]
+
+            start = numpy.insert(start[0], i + 1, tmp, axis = 0)
+
+            start = start[:-1,:]
+
+            start = numpy.expand_dims(start, axis=0)
+
+        tirage.append(f)
+        tirages.append(tirage[1:])
+
+    tirages_path = os.path.join(os.path.realpath("."), "tirages")
+    tirages_name = "tirages_" + datetime.now().strftime("%Y_%m_%d-%H_%M_%S") + ".json"
+
+    with open(os.path.join(tirages_path, tirages_name), 'w') as outfile:
+        json.dump(str(tirages), outfile)
